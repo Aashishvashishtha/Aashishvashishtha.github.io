521a0e367c6131aba257b0fb855efbdad5f21720
Aashishvashishtha <vashishthaa7@gmail.com>
2019-06-11 21:50:40 +0530

    project add
    whole portfolio

---
+++ precache-manifest.1ad4401e8ea6f42d58f9ae981fd7f2ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3585c6725b874d8a1663",
+    "url": "/static/js/main.08293d3b.chunk.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "3585c6725b874d8a1663",
+    "url": "/static/css/main.93e087e1.chunk.css"
+  },
+  {
+    "revision": "5cbd66207ef7879872bfb5c8932f33cd",
+    "url": "/index.html"
+  }
+];
